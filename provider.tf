a858108d47d656f2d58be67124190b247ee2633a
harikiranboye <harikiran.boyi@gmail.com>
2023-08-08 16:18:26 -0700

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -11,7 +11,7 @@ terraform {
   required_version = ">= 0.14.0"
 
   backend "s3" {
-    bucket = "ase-terraform-state-bucket"
+    bucket = "hboye-hboye"
     key    = "terraform.tfstate"
     region = "us-west-2"
   }
